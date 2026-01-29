431f8808c64bc6b8702d5a69d2da9f2e1e839a2a
Rafael Soares <rafaelsoares0501@gmail.com>
2026-01-29 20:00:41 -0300

    Aula 14

---
+++ aula14/ambiente.js
@@ -0,0 +1,17 @@
+/*
+var c = 1
+while (c <= 5) {
+    console.log(c)
+    c++
+}
+
+var r = 1
+do {
+    console.log(r)
+    r++
+}while (r <= 5)
+*/
+
+for(var c = 1; c <= 5; c++){
+    console.log(c)
+}
